6e25a58f5130747382744278a19df220b471f496
seeyeon <71398979+seeyeon@users.noreply.github.com>
2022-12-11 00:38:47 +0900

    first commit

---
+++ src/main/java/com/webserverproject/ProjectBatchApplication.java
@@ -0,0 +1,13 @@
+package com.webserverproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBatchApplication.class, args);
+	}
+
+}
